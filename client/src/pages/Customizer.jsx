5b0fa1bd2821bbd1bf2ddebb3538a5a4fb8bb495
benjamin-nartey <nartbillionz@gmail.com>
2023-08-04 19:43:51 +0000

    change port from dev to production in server

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -56,13 +56,16 @@ function Customizer() {
     try {
       setGeneratingImage(true);
 
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ prompt }),
-      });
+      const response = await fetch(
+        "https://threejs-openai-project-xnyu.onrender.com/api/v1/dalle",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ prompt }),
+        }
+      );
 
       const data = await response.json();
       console.log(data);
